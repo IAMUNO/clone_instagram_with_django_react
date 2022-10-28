bce3bea6031a95930c4090969d9f1696026f09f6
Yoo Yunho <yyh3690@naver.com>
2022-10-28 18:32:18 +0900

    PostNew 기본 화면 구현

---
+++ frontend/src/components/PostNewForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { Form, Input, Button } from "antd";
+
+
+export default function PostNewForm() {
+    const [fieldErrors, setFieldErrors] = useState({});
+    const handleFinish = () => {};
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+    };
+
+    const tailLayout = {
+        wrapperCol: { offset:8, span: 16 },
+    };
+
+    return (
+        <Form
+            {...layout}
+            onFinish={handleFinish}
+//                onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                    { required: true, message: 'Please input your username!' },
+                    { min: 5, message: "Please input more than 5 letters!" },
+                ]}
+                hasFeedback
+                {...fieldErrors.username}
+
+
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                    { required: true, message: 'Please input your password!' },
+                ]}
+                hasFeedback
+                {...fieldErrors.password}
+
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+
+        </Form>
+    )
+
+}
--- frontend/src/pages/PostNew.js
+++ frontend/src/pages/PostNew.js
@@ -1,66 +1,14 @@
-import React, { useState } from 'react';
-import { Card, Form, Input, Button } from "antd";
-
+import React  from 'react';
+import { Card } from "antd";
+import PostNewForm from 'components/PostNewForm';
+import "./PostNew.scss";
 
 export default function PostNew() {
-    const [fieldErrors, setFieldErrors] = useState({});
-    const handleFinish = () => {};
-
-    const layout = {
-        labelCol: { span: 8 },
-        wrapperCol: { span: 16 },
-    };
-
-    const tailLayout = {
-        wrapperCol: { offset:8, span: 16 },
-    };
-
     return (
-        <Card title="login">
-            <Form
-                {...layout}
-                onFinish={handleFinish}
-//                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        { required: true, message: 'Please input your username!' },
-                        { min: 5, message: "Please input more than 5 letters!" },
-                    ]}
-                    hasFeedback
-                    {...fieldErrors.username}
-
-
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[
-                        { required: true, message: 'Please input your password!' },
-                    ]}
-                    hasFeedback
-                    {...fieldErrors.password}
-
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item {...tailLayout}>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-
-            </Form>
-        </Card>
-
+        <div className="PostNew">
+            <Card title="New Posting">
+                <PostNewForm />
+            </Card>
+        </div>
     )
-
 }
