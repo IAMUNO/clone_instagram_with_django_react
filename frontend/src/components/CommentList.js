6ae5ebff63571e10f138489a6c483b116c74a483
Yoo Yunho <yyh3690@naver.com>
2022-11-02 17:31:13 +0900

    comment 세부 detail 구구현

--- frontend/src/components/CommentList.js
+++ frontend/src/components/CommentList.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppContext } from "store";
-import { Avatar, Comment, Tooltip } from 'antd';
+import { Button, Input } from 'antd';
 import moment from "moment";
 import Axios from 'axios';
 import useAxios from 'axios-hooks';
+import Comment from "./Comment";
 
 
 export default function CommentList({ post }) {
     const { store: { jwtToken } } = useAppContext();
+
+    const [commentContent, setCommentContent] = useState("");
 
     const headers = { Authorization: `Bearer ${jwtToken}` };
 
@@ -16,44 +19,44 @@ export default function CommentList({ post }) {
         headers
     });
 
+    const handleCommentSave = async () => {
+        const apiUrl = `http://127.0.0.1:8000/api/posts/${post.id}/comments/`
+
+
+        try {
+            const response = await Axios.post(
+                apiUrl,
+                { message: commentContent },
+                { headers }
+            );
+            setCommentContent("");
+            refetch();
+
+        }
+        catch(error) {
+        }
+    };
 
     return (
         <div>
-            <Comment
-                author={"Yunho Yoo"}
-                avatar={
-                    <Avatar
-                        src="https://joeschmoe.io/api/v1/random"
-                        alt="Han Solo"
-                    />
-                }
-                content={
-                    <p>comment content</p>
-                }
-                datetime={
-                    <Tooltip title="2016-11-22 11:22:33">
-                        <span>8 hours ago</span>
-                    </Tooltip>
-                }
-            />
+            {commentList && commentList.map(comment => (
+                <Comment key={comment.id} comment={comment} />
+            ))}
 
-                        <Comment
-                author={"Yunho Yoo"}
-                avatar={
-                    <Avatar
-                        src="https://joeschmoe.io/api/v1/random"
-                        alt="Han Solo"
-                    />
-                }
-                content={
-                    <p>comment content</p>
-                }
-                datetime={
-                    <Tooltip title="2016-11-22 11:22:33">
-                        <span>8 hours ago</span>
-                    </Tooltip>
-                }
+
+            <Input.TextArea
+                style={{ marginBottom: ".5em" }}
+                value={commentContent}
+                onChange={e => setCommentContent(e.target.value)}
             />
+            <Button
+                block
+                type="primary"
+                disabled={commentContent.length === 0}
+                onClick={handleCommentSave}
+            >
+                New Comment
+            </Button>
 
         </div>
     );
